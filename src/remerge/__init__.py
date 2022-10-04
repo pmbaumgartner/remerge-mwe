323587e4f5499f49534f0d7e4e943aa9d424e2bc
Peter Baumgartner <5107405+pmbaumgartner@users.noreply.github.com>
2022-10-03 20:25:19 -0400

    update version

--- src/remerge/__init__.py
+++ src/remerge/__init__.py
@@ -1,5 +1,5 @@
 from .core import run, SelectionMethod
 
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 
 __all__ = ["run", "SelectionMethod"]
--- tests/test_remerge.py
+++ tests/test_remerge.py
@@ -7,7 +7,7 @@ from .fixtures import sample_corpus
 
 
 def test_version():
-    assert __version__ == "0.1.1"
+    assert __version__ == "0.2.0"
 
 
 def test_single_iter(sample_corpus):
